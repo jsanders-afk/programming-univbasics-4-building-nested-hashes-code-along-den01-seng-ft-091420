024e6df8d088f5d0a20b432945b3fb612a0a42ad
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-01 04:05:10 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -4,9 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        friend1 = {age: "17",attitued:"worried",name:"Benvolio"},
-        friend2 = {age: "18",attitued:"hot-headed"}
+      :hero_friends => [{age: "17",attitued:"worried",name:"Benvolio"}, {age: "18",attitued:"hot-headed"}
       ]
    }, 
    :capulet => {
